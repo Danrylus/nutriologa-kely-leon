bb90f675678954b47e214eb769d1f74dbf770d9d
Danrylus <danrylus@gmail.com>
2023-04-18 00:24:21 -0500

    Agregada nueva navbar responsibe

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,26 +1,28 @@
 const toggleMenuBtn = document.getElementById('toggle-menu');
-const menuContainer = document.getElementById('menu-container');
+const menuContainer = document.getElementById('navbarNavAltMarkup');
 
-toggleMenuBtn.addEventListener('click', () => {
-    menuContainer.classList.toggle('show');
-});
-
-function closeMenu() {
-    menuContainer.classList.remove('show');
+function showMenu() {
+  var menu = document.getElementById("navbarNavAltMarkup");
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+  } else {
+    menu.classList.add("show");
+  }
 }
 
+
 function toggleSection(id) {
-    const selectedSection = document.getElementById(id);
-    if (selectedSection.style.height) {
-      selectedSection.style.height = null;
-    } else {
-      selectedSection.style.height = selectedSection.scrollHeight + 'px';
-    }
+  const selectedSection = document.getElementById(id);
+  if (selectedSection.style.height) {
+    selectedSection.style.height = null;
+  } else {
+    selectedSection.style.height = selectedSection.scrollHeight + 'px';
   }
-  
+}
+
 function scrollToSection(id) {
-    const section = document.getElementById(id);
-    section.scrollIntoView({
-        behavior: 'smooth'
-    });
+  const section = document.getElementById(id);
+  section.scrollIntoView({
+    behavior: 'smooth'
+  });
 }
